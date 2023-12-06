4c3d603757359e959decc69e6ae10662a8fee7d1
Yann Kamche <yannkamche@gmail.com>
2023-12-06 23:34:34 +0100

    Practicing Loops

--- Chapter 2/conditional.js
+++ Chapter 2/conditional.js
@@ -1,5 +1,21 @@
-//Conditional executin is created with the if keyword in JavaScript
-let theNumber = 3;
+//Conditional execution is created with the if keyword in JavaScript
+let theNumber = NaN;
 if (!Number.isNaN(theNumber)) {
     console.log("Your Number is the square root of " + theNumber ** 2)
-}
+} 
+
+//While loop: Runs a piece of code multiple times as long as a condition is satified
+//function to calculate power of a number using recursion
+const power = (base, exp) => {
+    return (exp === 0) ? 1 : base * power(base, exp - 1);
+};
+console.log(power(2, 10));
+
+//while loop implementation 
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+    result *= 2;
+    counter++
+};
+console.log(result)
