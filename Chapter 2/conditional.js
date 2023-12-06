a3d6a2190db476fbf34b7af104915f4531c3a412
Yann Kamche <yannkamche@gmail.com>
2023-12-06 23:50:05 +0100

    Practicing for loops

--- Chapter 2/conditional.js
+++ Chapter 2/conditional.js
@@ -4,18 +4,23 @@ if (!Number.isNaN(theNumber)) {
     console.log("Your Number is the square root of " + theNumber ** 2)
 } 
 
-//While loop: Runs a piece of code multiple times as long as a condition is satified
 //function to calculate power of a number using recursion
 const power = (base, exp) => {
     return (exp === 0) ? 1 : base * power(base, exp - 1);
 };
 console.log(power(2, 10));
 
-//while loop implementation 
+//WHILE LOOP: Runs a piece of code multiple times as long as a condition is satified
 let result = 1;
 let counter = 0;
 while (counter < 10) {
     result *= 2;
-    counter++
+    counter++;
 };
-console.log(result)
+console.log(result);
+
+//The role or the indentation is to make the structure of the code stand out
+
+//FOR LOOPS: Allows you to repeat a certain block of code for a known amount of time
+for (let number = 0; number <= 12; number += 2) 
+    console.log(number);
