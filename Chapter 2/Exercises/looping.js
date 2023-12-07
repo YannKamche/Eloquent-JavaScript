8fb169d74da7cd092c6393d8f2c7cc87270d9633
Yann Kamche <yannkamche@gmail.com>
2023-12-07 19:30:29 +0100

    Completed the FizzBuzz and the Looping exercise

---
+++ Chapter 2/Exercises/looping.js
@@ -0,0 +1,19 @@
+/**
+ * LOOPING A TRIANGLE
+Write a loop that makes seven calls to console.log to output the following
+triangle:
+#
+##
+###
+####
+#####
+######
+#######
+ */
+
+let value = "";
+
+for (let i = 1; i <= 7; i++) {
+    value += "H";
+    console.log(value);
+}
